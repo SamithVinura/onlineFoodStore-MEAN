9aedba96a1c2e79f079e387df1c0e375167fc52d
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-05-19 09:17:56 +0530

    add to cart button impemented

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/food';
 
@@ -11,7 +12,7 @@ import { Food } from 'src/app/shared/models/food';
 export class FoodPageComponent implements OnInit {
 
   food!:Food
-  constructor(private activatedRoute:ActivatedRoute, private foodService:FoodService){
+  constructor(private activatedRoute:ActivatedRoute, private foodService:FoodService,private cartService:CartService,private router:Router){
     this.activatedRoute.params.subscribe((params)=>{
       if(params.id){
         this.food = this.foodService.getFoodById(params.id)
@@ -21,6 +22,11 @@ export class FoodPageComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 
 }
--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Cart } from '../shared/models/Cart';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Food } from '../shared/models/food';
+import { CartItem } from '../shared/models/CartItem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  private cart:Cart = new Cart()
+  private cart:Cart = this.getCartFromLocalStorage()
   private cartSubject :BehaviorSubject<Cart> = new BehaviorSubject(this.cart)
 
   constructor() { }
@@ -18,11 +19,15 @@ export class CartService {
       .find(item => item.food.id === food.id);
     if (cartItem)
       return;
+
+    this.cart.items.push(new CartItem(food));
+    this.setCartToLocalStorage();
   }
 
   removeFromCart(foodId: string): void {
     this.cart.items = this.cart.items
       .filter(item => item.food.id != foodId);
+    this.setCartToLocalStorage();
   }
 
   changeQuantity(foodId: string, quantity: number) {
@@ -32,15 +37,36 @@ export class CartService {
 
     cartItem.quantity = quantity;
     cartItem.price = quantity * cartItem.food.price;
-
+    this.setCartToLocalStorage();
   }
 
   clearCart() {
     this.cart = new Cart();
+    this.setCartToLocalStorage();
   }
 
   getCartObservable(): Observable<Cart> {
     return this.cartSubject.asObservable();
+  }
+
+  getCart(): Cart{
+    return this.cartSubject.value;
+  }
+
+  private setCartToLocalStorage(): void {
+    this.cart.totalPrice = this.cart.items
+      .reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
+    this.cart.totalCount = this.cart.items
+      .reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0);
+
+    const cartJson = JSON.stringify(this.cart);
+    localStorage.setItem('Cart', cartJson);
+    this.cartSubject.next(this.cart);
+  }
+
+  private getCartFromLocalStorage(): Cart {
+    const cartJson = localStorage.getItem('Cart');
+    return cartJson ? JSON.parse(cartJson) : new Cart();
   }
 
 }
