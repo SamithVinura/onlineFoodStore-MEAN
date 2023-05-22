245c51b490cf8a7e34ff84d07fe3979eedfb2df5
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-05-22 07:58:37 +0530

    cart quantity dynamically loaded in header done

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  cartQuantity=0
+
+  constructor(private cartService:CartService){
+    this.cartService.getCartObservable().subscribe((newcart)=>{
+      this.cartQuantity = newcart.totalCount
+    })
+  }
+
+  ngOnInit():void{}
 
 }
