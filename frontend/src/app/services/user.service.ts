26d296932c2e96a0ef0420886d29a3c1c3fbd3c9
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-05-23 08:58:32 +0530

    login method done in service file

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../shared/models/user';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private userSubject = new BehaviorSubject<User>(new User())
+  public userObservable!:Observable<User>
+  constructor(private http:HttpClient) {
+    this.userObservable = this.userSubject.asObservable()
+  }
+
+
+
+  login(userLogin:IUserLogin):Observable<User>{
+
+    return  this.http.post<User>(USER_LOGIN_URL,userLogin)
+  }
 
 
 }
