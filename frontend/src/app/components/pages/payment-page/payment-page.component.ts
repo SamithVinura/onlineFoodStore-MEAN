a52598308240d348b6d2445fc948764627f8ab47
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-06-01 08:55:02 +0530

    create get method in payment.ts file and service file

--- frontend/src/app/components/pages/payment-page/payment-page.component.ts
+++ frontend/src/app/components/pages/payment-page/payment-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-payment-page',
   templateUrl: './payment-page.component.html',
   styleUrls: ['./payment-page.component.css']
 })
-export class PaymentPageComponent {
+export class PaymentPageComponent implements OnInit {
 
+  order:Order = new Order()
+  constructor(private orderService:OrderService,private router:Router){
+    this.orderService.getNewOrderForCurrentUser().subscribe({
+      next:(order)=>{
+        this.order = order
+      },
+      error:()=>{
+        this.router.navigateByUrl('/checkout')
+      }
+    })
+  }
+
+  ngOnInit():void{}
 }
